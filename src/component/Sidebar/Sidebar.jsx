ec2872908eb2c1d6a3166ed69165f4f3c83e115f
jiya10208 <jiya102082004@gmail.com>
2025-05-28 05:49:03 +0530

    sidebar jsx and acss added. Font-outfit to index.html

--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -1,34 +1,46 @@
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
+import { useState } from "react";
 export default function Sidebar() {
+  const [extended, setExtended] = useState(false);
+
   return (
     <div className="sidebar">
       <div className="top">
-        <img src={assets.menu_icon} alt="" className="menu" />
+        <img
+          src={assets.menu_icon}
+          alt="Menu icon"
+          className="menu"
+          onClick={() => setExtended(!extended)}
+        />
+
         <div className="new-chat">
           <img src={assets.plus_icon} alt="" />
-          <p>New Chat</p>
+          {extended ? <p>New Chat</p> : null}
         </div>
-        <div className="recent">
-          <p className="recent-title">Recent</p>
-          <div className="recent-entry">
-            <img src={assets.message_icon} alt="" />
-            <p>what is react ...</p>
+
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>what is react...</p>
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
       <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="" />
-          <p>Help</p>
+          {extended ? <p>Help</p> : null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="" />
-          <p>Activity</p>
+          {extended ? <p>Activity</p> : null}
         </div>
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="" />
-          <p>Settings</p>
+          {extended ? <p>Settings</p> : null}
         </div>
       </div>
     </div>
